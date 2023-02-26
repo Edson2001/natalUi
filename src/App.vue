b0ef27217898c363f4aa20bbb4546ba2dd6548a7
edson2001 <djiedson413@gmail.com>
2023-02-26 08:34:10 +0100

    fix: ajusts on layout

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,9 @@ import Button from "./components/Button.vue"
 
           <h3>
             Feliz Natal e
+            Feliz Ano Novo!
           </h3>
-          <h3>Feliz Ano Novo!</h3>
+         
           <p>Nessas festas de fim de ano mande um presente para a pessoa amada e compartilhe a alegria do Natal.</p>
           <Button >
             Fazer uma doação
@@ -42,20 +43,34 @@ import Button from "./components/Button.vue"
     <div class="boxs">
 
       <div class="box">
+        <img src="/images/giving1.svg" alt="">
+       <div class="boxText">
         <h6>Presentes</h6>
         <p>Eles são os melhores presentes que existem.</p>
+       </div>
       </div>
       <div class="box">
-        <h6>Presentes</h6>
-        <p>Eles são os melhores presentes que existem.</p>
+        <img src="/images/giving2.svg" alt="">
+       <div class="boxText">
+        <h6>Enfeites</h6>
+        <p>Dê uma decoração.</p>
+       </div>
       </div>
+
       <div class="box">
-        <h6>Presentes</h6>
-        <p>Eles são os melhores presentes que existem.</p>
+        <img src="/images/giving4.svg" alt="">
+       <div class="boxText">
+        <h6>Doces</h6>
+        <p>Aproveite os doces natalinos.</p>
+       </div>
       </div>
+
       <div class="box">
-        <h6>Presentes</h6>
-        <p>Eles são os melhores presentes que existem.</p>
+        <img src="/images/giving3.svg" alt="">
+       <div class="boxText">
+        <h6>Muito amor</h6>
+        <p>Aproveite os sentimentos que duram para sempre.</p>
+       </div>
       </div>
 
     </div>
@@ -94,12 +109,20 @@ import Button from "./components/Button.vue"
       display: flex;
       align-items: center;
       justify-content: space-between;
-      
+      h3{
+        color: #161212;
+        font-weight: 600;
+        font-size: 48px;
+        max-width: 380px;
+      }
       p{
         margin-top: 12px;
         margin-bottom: 20px;
         color: #404040;
         width: 446px;
+      }
+      .contentHeader{
+        width: 100%;
       }
     }
   }
@@ -128,6 +151,8 @@ import Button from "./components/Button.vue"
       align-items: center;
       width: 100%;
       flex-wrap: wrap;
+      height: auto;
+      margin-bottom: 290px;
       .box{
         height: 100px;
         flex: 1 0 200px;
@@ -135,6 +160,16 @@ import Button from "./components/Button.vue"
         text-align: center;
         &:last-child{
           margin-right: 0;
+        }
+        h6{
+          font-weight: 500;
+          margin-bottom: 12px;
+          font-size: 20px;
+        }
+        p{
+          color: #8D8D8D;
+          font-size: 16px;
+          font-weight: 500;
         }
         p, h6{
           display: block;
@@ -144,6 +179,7 @@ import Button from "./components/Button.vue"
 
   .lastSection{
     background-color: #f7c7b9;
+    height: auto;
     .contentLast{
       display: flex;
       justify-content: space-between;
@@ -151,11 +187,20 @@ import Button from "./components/Button.vue"
       h3{
         font-size: 43px;
         font-weight: bold;
-        p{
+        max-width: 400px;
+        width: 100%;
+        margin-bottom: 24px;
+      }
+      p{
           color: #404040;
           font-size: 16px;
+          max-width: 400px;
+          width: 100%;
+          margin-bottom: 32px;
         }
-      }
+    }
+    .contentLastSection{
+      width: 100%;
     }
   }
 </style>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -4,6 +4,7 @@
     <nav>
 
         <h1>
+            <img src="/images/Frame.svg" alt="">
             <a href="">Feliz Natal</a>
         </h1>
         <ul>
@@ -35,9 +36,22 @@ nav{
         align-items: center;
         li{
             margin-right: 22px;
+            font-weight: 500;
         }
     }
-    
+    a{
+        color: #161212;
+    }
+    h1{
+        font-weight: 500;
+        font-size: 24px;
+        line-height: 36px;
+        display: flex;
+        align-items: center;
+        img{
+            margin-right: 12px;
+        }
+    }
     padding: 10px;
 
 }
