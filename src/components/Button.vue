bbddf97b0e2a5965ffc577bff0c776890d892c1e
edson2001 <djiedson413@gmail.com>
2023-02-26 08:33:57 +0100

    fix: ajusts on button compoment

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,7 @@
 <template>
 
     <button class="myButton">
+        <img src="/images/gift.svg" alt="">
         <slot></slot>
     </button>
 
@@ -14,6 +15,20 @@
         color: #FFF;
         border-radius: 8px;
         padding: 10px;
+        font-size: 16px;
+        font-weight: 600;
+        &:hover{
+            cursor: pointer;
+        }
+        display: flex;
+        align-items: center;
+
+        justify-content: space-between;
+        img{
+            width: 20px;
+            height: 20px;
+            margin-right: 12px;
+        }
     }
 
 </style>
